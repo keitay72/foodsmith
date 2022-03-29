a3a83e930f892946b6c251a3a756865782852a11
keitay72 <keitay72@hotmail.com>
2022-03-28 23:31:31 -0500

    Ingredient search list - add and remove ingredients

--- src/components/IngredientSearchModal/IngredientSearchModal.jsx
+++ src/components/IngredientSearchModal/IngredientSearchModal.jsx
@@ -3,9 +3,53 @@ import "./ingredientSearchModal.css";
 import logo from "../../assets/food.ico";
 import { AiFillCloseCircle } from "react-icons/ai";
 import { useModal } from "../../contexts/ModalContext";
+import { BsPlusSquareFill } from "react-icons/bs";
 
 const IngredientSearchModal = () => {
   const { setShowModal } = useModal();
+
+  const [searchIngredient, setIngredient] = React.useState("");
+  const [ingredientsSearchList, setIngredientsSearchList] = React.useState([]);
+
+  const handleIngredient = (e) => {
+    e.preventDefault();
+
+    setIngredient(e.target.value.toLowerCase());
+  };
+
+  const addToSearchList = (e) => {
+    e.preventDefault();
+
+    const newIngredients = [...ingredientsSearchList];
+
+    if (!searchIngredient) {
+      return;
+    } else {
+      newIngredients.push(searchIngredient);
+    }
+
+    if (ingredientsSearchList.includes(searchIngredient)) {
+      alert("Duplicate Ingredient");
+    }
+
+    if (!ingredientsSearchList.includes(searchIngredient))
+      setIngredientsSearchList(newIngredients);
+
+    setIngredient("");
+  };
+
+  const handleRemoveIngredient = (food) => {
+    const element = document.getElementById(food);
+
+    const ingredient = element.innerText;
+
+    const newIngredients = ingredientsSearchList.filter(
+      (item) => item !== ingredient
+    );
+
+    setIngredientsSearchList(newIngredients);
+  };
+
   return (
     <div className="modal-bg" onClick={() => setShowModal(false)}>
       <div className="ing-modal" onClick={(e) => e.stopPropagation()}>
@@ -19,10 +63,46 @@ const IngredientSearchModal = () => {
         </header>
         <div className="left"></div>
         <div className="right">
-          3<sup>1</sup>&frasl;<sub>3</sub> Cups
-          <br />3 1&frasl;3 Cups
-          <br />
-          Preheat oven to 300&deg;.
+          <form
+            className="ingredient-search-form"
+            action="submit"
+            onSubmit={addToSearchList}>
+            <div className="ingredient-search-input-div">
+              <input
+                id="indredient"
+                type="text"
+                onChange={handleIngredient}
+                value={searchIngredient}
+              />
+              <button className="ingredient-search-add-btn">
+                <BsPlusSquareFill className="ingredient-search-add" />
+              </button>
+            </div>
+            {console.log(searchIngredient)}
+            {console.log(ingredientsSearchList)}
+            <div className="ingredients-list-div">
+              {ingredientsSearchList.length
+                ? ingredientsSearchList.map((ingredient) => {
+                    return (
+                      <div className="ingredient-keyword-div" key={ingredient}>
+                        <h4 className="ingredient-keyword" id={ingredient}>
+                          {ingredient}
+                        </h4>
+                        <AiFillCloseCircle
+                          className="ingredient-keyword-icon"
+                          onClick={() => handleRemoveIngredient(ingredient)}
+                        />
+                      </div>
+                    );
+                  })
+                : ""}
+            </div>
+            {/* This button styling is primarily in "loginSignup.css"
+            Some addition styling for positioning is in "ingredientSearchModal.css" */}
+            <button className="ingredient-search-btn" type="button">
+              Search
+            </button>
+          </form>
         </div>
       </div>
     </div>
