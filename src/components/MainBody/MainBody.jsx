9055f15bed377b35f2d4c129fbda7477dcaa895d
keitay72 <keitay72@hotmail.com>
2022-04-24 19:04:36 -0500

    RecipeTile and MainBody
    RecipeTiles now display to MainBody

--- src/components/MainBody/MainBody.jsx
+++ src/components/MainBody/MainBody.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import "./mainBody.css";
 import RecipeTile from "../RecipeTile/RecipeTile";
+import data from "../../database/db.json";
 
 const MainBody = () => {
+  const recipes = data.recipes;
+
   return (
-    <div>
-      <RecipeTile />
+    <div className="main-body">
+      <div className="main-body-row">
+        {recipes.map((recipe) => {
+          return (
+            <RecipeTile
+              key={recipe.id}
+              title={recipe.title}
+              image={recipe.image}
+              subtitle={recipe.subtitle}
+              course={recipe.course}
+              category={recipe.category}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/components/RecipeTile/RecipeTile.jsx
+++ src/components/RecipeTile/RecipeTile.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./recipeTile.css";
 
-const RecipeTile = () => {
-  return <div>RecipeTile</div>;
+const RecipeTile = ({ key, title, image, subtitle, course, category }) => {
+  const capitalFirstLeter = (word) => {
+    if (typeof word === "string") {
+      return word.replace(/^\w/, (c) => c.toUpperCase());
+    }
+    return word;
+  };
+
+  return (
+    <div className="recipe-tile" key={key}>
+      <h5>{capitalFirstLeter(course)}</h5>
+      <h3>{title}</h3>
+      <img src={image} />
+      <h3>{subtitle}</h3>
+      <h3>{category}</h3>
+    </div>
+  );
 };
 
 export default RecipeTile;
